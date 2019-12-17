caa6506bb3a8198062fe194347d70d84d35a6fb4
nonomu <54757629+nonomu@users.noreply.github.com>
2019-12-17 13:34:38 +0200

    adding basic sql for building tables

---
+++ src/sqlBuildDB/insertTables.sql
@@ -0,0 +1,31 @@
+use weddingplanner;
+
+-- INSERT INTO weddingDetails
+--  VALUES (NULL,"Noam Mualmi",
+-- "Sapir Mi",'1995-11-28',500,100000
+-- ,120000,"Jerusalem","Pop");
+
+
+-- INSERT INTO user
+--  VALUES (NULL,"noammualmi@gmail.com","nono123",
+--  1);
+
+
+-- INSERT INTO attractions
+--  VALUES (
+--      NULL,
+--      "venue",
+--      "Venetion hall",
+--      "Noam",
+--      "https://www.venetian-israel.co.il/public/Winter2020/images/gallery_7.png",
+--      "Petah-Tikva",
+--      4.5,
+--      "Dudu",
+--      0722133329,
+--      "venetion@gmail.com",
+--      "hello"
+--      );
+
+
+-- INSERT INTO booked_attractions
+--  VALUES (1,1,200);
