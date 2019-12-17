8384dcb4eca07d44febd0ea55b31e10ae599fcc2
Ron Braha <Ron.Braha@icloud.com>
2019-12-17 14:11:29 +0200

    work on user routes

--- src/stores/User.js
+++ src/stores/User.js
@@ -1,8 +1,12 @@
 import {observable, action, computed} from 'mobx'
 
 class User {
+    
+    
     @observable userInfo = []
     @observable closedAttractions = []
+
+    @action login
 }
 
 export const user = new User()
