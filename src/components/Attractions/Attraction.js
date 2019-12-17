13d2c283a13ed1e9325cb2e485348839d45678c5
ori <oritsadok1@gmail.com>
2019-12-17 14:11:38 +0200

    m

--- src/components/Attractions/Attraction.js
+++ src/components/Attractions/Attraction.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-
+import { observer,inject } from "mobx-react";
+@inject("attractions")
+@observer
 class Attraction extends Component {
 render() {
-return <h4>This is ATTRACTION (Ori, work on this with the dummy data)</h4>
+  return 
+  ( <div className="attraction-container">
+ <h4>This is ATTRACTION (Ori, work on this with the dummy data)</h4>
+    </div>)
 }
 }
 
--- src/components/Attractions/Attractions.js
+++ src/components/Attractions/Attractions.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Attraction from './Attraction';
-
+import { observer, inject } from "mobx-react";
+@inject("attractions")
+@observer
 class Attractions extends Component {
 render() {
+    let venue = this.props.attractions.venue
+
 return <div>
     <h1>Attractions Page</h1>
     this page should print here the route match
 
     
-    <Attraction />
+    <Attraction 
+ />
     <Attraction />
     </div>
 }
