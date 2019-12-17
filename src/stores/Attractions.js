35436d9dea56192499836385017bca8a3e1431c9
Ron Braha <Ron.Braha@icloud.com>
2019-12-17 13:34:24 +0200

    stores-route connection

--- src/stores/Attractions.js
+++ src/stores/Attractions.js
@@ -1,15 +1,54 @@
 import {observable, action, computed} from 'mobx'
+import axios from 'axios'
+let API_URL = `http://localhost:4200/api`
 
 class Attractions {
     @observable _attractions = {
         venue: [],
         dj: [],
         photographer: [],
-        general: []
+        misc: []
     }
+    @observable _userFavorites = {
+        venue: [],
+        dj: [],
+        photographer: [],
+        misc: []
+    }
+
     @computed get attractions() {
         return this._attractions
     }
+
+    @action getAttractionsByCategory = async categoryName => {
+        try {
+            let attractions = await axios.get(`${API_URL}/attractions/${categoryName}`)
+            this._attractions[categoryName] = attractions.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    @action getUserFavorites = async userId => {
+        try {
+            let userFavorites = await axios.get(`${API_URL}/favorites/${userId}`)
+            this._userFavorites = userFavorites.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+ 
+    @action bookAttraction = async (userId, attractionId) => {
+        try {
+            await axios.post(`${API_URL}/attractions/book`, {userId, attractionId})
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    // @action addToFavorites = async (user)
+
+    
 }
 
 export const attractions = new Attractions()
