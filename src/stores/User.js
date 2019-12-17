192527ab0dac6257a307007e58e13e64ea17db54
Ron Braha <Ron.Braha@icloud.com>
2019-12-17 15:44:12 +0200

    GET by category, implemented login

--- src/stores/User.js
+++ src/stores/User.js
@@ -1,12 +1,32 @@
 import {observable, action, computed} from 'mobx'
+import Axios from 'axios'
+let API_URL = `http://localhost:4200/api`
 
 class User {
-    
-    
     @observable userInfo = []
     @observable closedAttractions = []
 
-    @action login
+    @action login = async (email, password) => {
+        try {
+            let user = await Axios.post(`${API_URL}/login`, {email, password})
+            this.getUserInfo(user.data.id)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    @action getUserInfo = async userId => {
+        try {
+            let userInfo = await Axios.get(`${API_URL}/wedding-details/${userId}`)
+            this.userInfo = userInfo.data
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    @action register = () => {
+
+    }
 }
 
 export const user = new User()
