4ff3eabd2bff966e2cf14b68543133276525bd74
Ron Braha <Ron.Braha@icloud.com>
2019-12-17 12:08:40 +0200

    provider + stores

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'mobx-react'
+import {attractions} from './stores/Attractions'
+import {attraction} from './stores/Attraction'
+import {user} from './stores/User'
 
-const stores = {}
+const stores = {attractions, attraction, user}
 
 ReactDOM.render(<Provider {...stores}><App /></Provider>, document.getElementById('root'));
 
---
+++ src/stores/User.js
@@ -0,0 +1,7 @@
+import {observable, action, computed} from 'mobx'
+
+class User {
+    
+}
+
+export const user = new User()
