d36e3a6f7465b437bc135d5c215f2118d5d4d5e8
nonomu <54757629+nonomu@users.noreply.github.com>
2019-12-17 17:14:53 +0200

    added bookedAtractions route

--- server/routes/api.js
+++ server/routes/api.js
@@ -47,11 +47,11 @@ router.get('/attractions/:userId', async function (req, res) {
 router.get('/bookedAtractions/:userId',async function (req, res) {
     let userId = req.params.userId
     try {
-        let bookedAtractions = await db.query(
+        let bookedAttractions = await db.query(
         `SELECT at.* 
         FROM attractions as at,user as u,booked_attractions as ba 
         WHERE ba.user_id = "${userId}" AND ba.attraction_id=at.id AND u.id = "${userId}"`)
-        res.send(bookedAtractions[0])
+        res.send(bookedAttractions[0])
     } catch(err) {
         console.log(err)
         res.send(err)
