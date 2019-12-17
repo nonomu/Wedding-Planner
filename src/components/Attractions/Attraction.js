ce11f7f4a256bd1e8f29abd7f5904116d1a1760f
ori <oritsadok1@gmail.com>
2019-12-17 15:49:11 +0200

    delete

--- src/components/Attractions/Attraction.js
+++ src/components/Attractions/Attraction.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { observer, inject } from "mobx-react";
-@inject("attractions")
+
 
 @observer
 class Attraction extends Component {
@@ -11,7 +11,7 @@ class Attraction extends Component {
 
         return (<div className="attraction-container">
             <h4>This is ATTRACTION (Ori, work on this with the dummy data)</h4>
-            <img className="attr-img" src={this.props.attr.image} />
+            <img className="attr-img" src={attraction.attr.image} />
             <div className="attr-name">name: {attraction.attr_name}</div>
             <div className="attr-vendor">vendor: {attraction.attr_vendor}</div>
             <div className="attr-rating">rating: {attraction.rating}</div>
