847e40178f3e94321651b13aec7ae2b696efa386
ori <oritsadok1@gmail.com>
2019-12-18 21:44:39 +0200

    fix favorites

--- src/components/User/Favorites.js
+++ src/components/User/Favorites.js
@@ -19,7 +19,7 @@ class Favorites extends Component {
         return (<div>
             <h1>Favorites</h1>
             {categories.map((c, i) => userFavorites
-                .map(uf => uf.category === c ? <Favorite attraction={uf} key={uf.name} /> : null))
+                .map(uf => uf.category === c ? <Attraction attr={uf} key={uf.name} /> : null))
             }
         </div>)
     }
