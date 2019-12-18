fe2cc689b009c8823d4cf33a53f38c2c0e1d6038
nonomu <54757629+nonomu@users.noreply.github.com>
2019-12-19 01:59:11 +0200

    fiixed add and remove favorites

--- src/components/Attractions/Attraction.js
+++ src/components/Attractions/Attraction.js
@@ -9,18 +9,6 @@ import { attractions } from '../../stores/Attractions';
 
 @observer
 class Attraction extends Component {
-    constructor()
-    {
-        super()
-        this.state={
-            bool:true
-        }
-    }
-    // componentDidMount=()=>
-    // {
-    //     this.props.user.removeFavorite
-    // }
-
     removeFavorite = async ()  =>{
       await this.props.user.removeFavorite(this.props.user.userInfo.id,this.props.attr.id)
       console.log("remove")
@@ -40,32 +28,11 @@ class Attraction extends Component {
     changeFavoriteState = ()=>{
         this.props.attractions.changeFavoriteState()
     }
-    checkIfExist=()=>
-    {
-        let favorites=this.props.user._userFavorites
-        if(favorites.some(f => f.id === this.props.attr.id))
-        {
-        if(this.state.bool===true)
-        return    
-        this.setState({bool: true})
-        }
-        else{
-            if(this.state.bool===false)
-            return 
-            this.setState({bool: false}) 
-        }
-    }
-
-    componentDidUpdate() {
-        // if(this.props.name==="attractions") 
-       this.checkIfExist()
-    }
-
     render() {
         let favorites=this.props.user._userFavorites
         let bool=favorites.some(f => f.id === this.props.attr.id)
         let attraction = this.props.attr
-        return ({attraction}?<AttractionCard bool={bool}attraction={attraction}removeFavorite={this.removeFavorite} addToFavorites={this.addToFavorites} changeFavoriteState={this.changeFavoriteState}/>:"")
+        return ({attraction}?<AttractionCard bool={bool} attraction={attraction}removeFavorite={this.removeFavorite} addToFavorites={this.addToFavorites} changeFavoriteState={this.changeFavoriteState}/>:"")
     }
 }
 
