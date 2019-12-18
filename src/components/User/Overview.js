04a85ab398be600908e8561a3dc0c93f1173614e
yanivsultan <yanivsultan5@gmail.com>
2019-12-18 21:29:03 +0200

    overview transfered to materalize ui

--- src/components/User/Overview.js
+++ src/components/User/Overview.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
+import Paper from './Paper'
 
 @inject("user")
 @observer
@@ -10,15 +11,28 @@ class Overview extends Component {
 
   render() {
     let bookedAttractions = this.props.user.bookedAttractions;
-    return (
-      <div >
-        {bookedAttractions.map(a => 
-        <div key={a.category} className="bookedAttraction">
-  <h2>{a.category}</h2><div>venue name: {a.attr_name} vendor name:{a.attr_vendor}</div><div>vendor contact name: {a.contact_name} vendor Phone: {a.contact_phone}</div><img className="smallIMG" src={a.image} alt={a.attr_name}/> <div>The price is: {a.price}</div></div>
-        )}
-        <p>add the attractions that the user needs to close :) </p>
-      </div>
-    );
+    return <div>
+      {bookedAttractions.map(ba => <Paper attr={ba} />)}
+    </div>
+    // (
+    //   <div>
+    //     {bookedAttractions.map(a => (
+    //       <div key={a.category} className="bookedAttraction">
+    //         <h2>{a.category}</h2>
+    //         <div>
+    //           venue name: {a.attr_name} vendor name:{a.attr_vendor}
+    //         </div>
+    //         <div>
+    //           vendor contact name: {a.contact_name} vendor Phone:{" "}
+    //           {a.contact_phone}
+    //         </div>
+    //         <img className="smallIMG" src={a.image} alt={a.attr_name} />{" "}
+    //         <div>The price is: {a.price}</div>
+    //       </div>
+    //     ))}
+    //     <p>add the attractions that the user needs to close </p>
+    //   </div>
+    // );
   }
 }
 
