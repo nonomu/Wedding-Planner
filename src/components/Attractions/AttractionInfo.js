072b62f236ea48811a26fd5c0ba7536a1512fb0b
ori <oritsadok1@gmail.com>
2019-12-18 21:05:46 +0200

    add attractionInfo component

--- src/components/Attractions/AttractionInfo.js
+++ src/components/Attractions/AttractionInfo.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react'
+@inject('user', 'attractions')
 
+@observer
 class AttractionInfo extends Component {
-render() {
-return 
-}
+    
+    render() {
+
+        return (
+            <div>
+                
+                    <h3>{a.attr_name}</h3>
+                    <div>vendor: {a.vendor}</div>
+                    <div>({a.category})</div>
+                    <img src={a.image} />
+                    <div>contact: {a.contact_name}  {a.contact_phone}  {a.contact_email}</div>
+                    <div>{a.location}</div>
+                    <div>rating: {a.rating}</div>
+                    <div>{a.small_prints}</div>
+
+                )}
+            </div>
+        );
+    }
 }
 
 
