f02ce93ea07199a1e2013cd64bf9941c24a87747
ori <oritsadok1@gmail.com>
2019-12-18 17:38:46 +0200

    add fileds

--- src/components/User/Favorite.js
+++ src/components/User/Favorite.js
@@ -13,8 +13,10 @@ class Favorite extends Component {
         return (
             <div>
                     <h3>{attraction.attr_name}</h3>
+                    <div>({attraction.category})</div>
                     <img src={attraction.image} />
                     <div>contact: {attraction.contact_name}  {attraction.contact_phone}  {attraction.contact_email}</div>
+                    <div>{attraction.location}</div>
                     <div>rating: {attraction.rating}</div>
                     <div>{attraction.small_prints}</div>
                     <button onClick={this.removeFavorite}>Delete From Your Favorites</button>
