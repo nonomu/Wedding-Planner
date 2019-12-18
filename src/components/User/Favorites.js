e72a3952e71c8f4cbb2d33c31a91a122551af94a
yanivsultan <yanivsultan5@gmail.com>
2019-12-18 22:50:09 +0200

    added grid to favorites

--- src/components/User/Favorites.js
+++ src/components/User/Favorites.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
 import Attraction from "../Attractions/Attraction";
+import './favorites.css'
 @inject("user", "attractions")
 
 
@@ -15,14 +16,18 @@ class Favorites extends Component {
         let userFavorites = this.props.user._userFavorites
         let categories = this.props.attractions.categories
     
-        return (<div>
-            <h1>Favorites</h1>
+        return (
+        <div>
+            <h1 className="attraction-title">Favorites</h1>
+            <div className="attractions">
             {categories.map((c, i) => userFavorites
 
                 .map(uf => uf.category === c ? <Attraction attr={uf} key={uf.name} /> : null))
 
             }
-        </div>)
+        </div>
+        </div>
+        )
     }
 }
 
