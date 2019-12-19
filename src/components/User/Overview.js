9297d6bbfec968b484fab75a282215fa8611981c
Ron Braha <Ron.Braha@icloud.com>
2019-12-19 06:21:38 +0200

    clean files

--- src/components/User/Overview.js
+++ src/components/User/Overview.js
@@ -17,7 +17,6 @@ class Overview extends Component {
   render() {
     let bookedAttractions = this.props.user.bookedAttractions;
     let totalPrice = bookedAttractions.reduce((a,b) => a+b.price,0)
-    console.log(totalPrice)
     let getWeddingDetails = this.props.user.userInfo
     let weddingDetailes = this.props.user.userInfo.weddingData ? this.props.user.userInfo.weddingData:""
     return <div>
--- src/stores/User.js
+++ src/stores/User.js
@@ -21,7 +21,6 @@ class User {
     @action updateUserInfo = async(profile)=>
     {
         profile.id=this.userInfo.id
-        console.log(profile)
         try {
             await Axios.put(`${API_URL}/update/UserInfo`, profile)
             
@@ -34,7 +33,6 @@ class User {
       try {
             let userInfo = await Axios.get(`${API_URL}/wedding-details/${this.userInfo.id}`)
             this.userInfo.weddingData = userInfo.data
-            console.log(userInfo)
         } catch (err) {
             console.log(err)
         }
@@ -43,8 +41,6 @@ class User {
   
   @action isFavorite(attr_id) {
     let bool=  this._userFavorites.some(a => a.id === attr_id);
-    console.log(this._userFavorites)
-    console.log(bool)
     return bool
   };
 
@@ -88,7 +84,6 @@ class User {
         attractionId,
         price
       });
-      console.log("OK")
     } catch (err) {
       console.log(err);
       
