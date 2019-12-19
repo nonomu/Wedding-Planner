b331418bfc3d0b9b359f035f1fa57da34afd1715
nonomu <54757629+nonomu@users.noreply.github.com>
2019-12-19 05:44:59 +0200

    a little grid stuff

--- src/components/User/Overview.js
+++ src/components/User/Overview.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 import Paper from './Paper'
 import { TextField } from '@material-ui/core';
-
+import './Overview.css'
 @inject("user")
 @observer
 class Overview extends Component {
@@ -18,7 +18,7 @@ class Overview extends Component {
     let getWeddingDetails = this.props.user.userInfo
     let weddingDetailes = this.props.user.userInfo.weddingData ? this.props.user.userInfo.weddingData:""
     return <div>
-      <div>
+      <div className="budget">
         Total Budgut:{weddingDetailes? weddingDetailes.est_budget:""} <br></br>
         Remaining Budget:{weddingDetailes.est_budget - totalPrice} <br></br>
         Total price Of Booked:{totalPrice}
