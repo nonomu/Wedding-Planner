79687acef680f66f3f13c0b90f1d3247b3660063
ori <oritsadok1@gmail.com>
2019-12-23 17:02:58 +0200

    f****

--- server/apigoogle.js
+++ server/apigoogle.js
@@ -4,28 +4,23 @@ const Sequelize = require('sequelize')
 const db = new Sequelize('mysql://root:@localhost/weddingPlanner')
 const requestPromise = require('request-promise')
 const apiKey = "AIzaSyAtbzpk-aq32pC5OBoeCmftPWhTNMthKOM"
+const axios =require('axios')
+//const Places= new GooglePlaces("../node_modules/google-places-web").default; 
 
 
 router.get('/', async function (req, res) {
     const relevantData = []
-  let requestedData = await requestPromise(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=dj+Tel%25Aviv+Israel&fields=name,rating,formatted_phone_number,photos&key=${apiKey}`)
-        let results = JSON.parse(requestedData).results
-        for (result of results) {
-            if (typeof (result.photos || result.geometry.location) == 'undefined') return
-            let coordinates = `http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20&minx=${result.geometry.location.lat}&miny${result.geometry.location.lng}&maxx=${result.geometry.location.lat}&maxy=${result.geometry.location.lng}&size=medium&mapfilter=true`
-            relevantData.push({
-                // lat: result.geometry.location.lat,
-                // lng: result.geometry.location.lng,
-                //  photo: photo,
-                coordinates:coordinates,
-                 name: result.name,
-                 rating: result.rating
-                })
-            }
+    let requestedData = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=dj+Tel%25Aviv+Israel&fields=name,rating,formatted_phone_number,photos&key=${apiKey}`)
+    let results = requestedData.data.results
+    await results.forEach(async (r) => {
+        if (typeof (r.photos) == 'undefined') return
+        let photo = await requestPromise(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${r.photos[0].photo_reference}&key=${apiKey}`,
+        function (error,response) {
+           console.log(response.request.uri.href)
+           // res.send(response)
+        })
 
-        res.send(relevantData)
-        
-        
     })
-    module.exports = router
-    //let photo = await requestPromise(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=${apiKey}`)
+           
+ })
+ module.exports = router
