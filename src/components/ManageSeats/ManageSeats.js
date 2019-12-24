3c95acd953d9eefe978981590d05968face3ae53
yanivsultan <yanivsultan5@gmail.com>
2019-12-24 12:40:33 +0200

    fixes component did mount fix

--- src/components/ManageSeats/ManageSeats.js
+++ src/components/ManageSeats/ManageSeats.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import AddInvitee from "./AddInvitee";
+import { observer } from "mobx-react";
 
+
+@observer
 class ManageSeats extends Component {
   render() {
     return (
