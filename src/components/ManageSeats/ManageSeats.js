6efceb70214719a1cc63cd35d7b70c71697e3909
yanivsultan <yanivsultan5@gmail.com>
2019-12-25 11:18:28 +0200

    working on manage seats

--- src/components/ManageSeats/ManageSeats.js
+++ src/components/ManageSeats/ManageSeats.js
@@ -1,30 +1,35 @@
 import React, { Component } from "react";
-import { observer, inject } from 'mobx-react'
+import { observer, inject } from "mobx-react";
 import AddInvitee from "./AddInvitee";
 import InviteesSideBar from "./invitees_side_bar";
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Table from "./Table";
 
-@inject('manage_seats')
-
+@inject("manage_seats", "user")
 @observer
 class ManageSeats extends Component {
+  async componentDidMount() {
+    await this.props.user.getWeddingDetails();
+    this.props.manage_seats.getTables(this.props.user.userInfo.weddingData.id);
+  }
 
   render() {
+    console.log(this.props.manage_seats.tables);
     return (
       <div id="manage_seats">
         <AddInvitee />
-        <div id="check">
-          <br/>
-          <h2>INVITEES</h2> <br/> <br/>
-          here the invitees will be displayed, need to decide if it will be a menu or just a box.
+        {/* <div id="check">
         <InviteesSideBar />
-
+        </div> */}
+        <div>
+          {this.props.manage_seats.tables.map(t => (
+            <Table t={t} />
+          ))}
         </div>
-
         <Fab id="addIcon" color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
+          <AddIcon />
+        </Fab>
       </div>
     );
   }
